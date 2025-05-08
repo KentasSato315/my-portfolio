cd3431d867c1918d374d3a9e0ad7676b4dcfa401
KentasSato315 <satou.ke.19920315@gmail.com>
2025-05-08 22:01:20 +0900

    デプロイ関連の修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
-    "./src/**/*.component.{html,ts}"
+    "./src/**/*.{html,ts}"
   ],
   theme: {
     extend: {},
